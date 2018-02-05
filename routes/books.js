850303eb032ff95277e47649d8bdd78d5c1e42d2
krishna_dixit <rkdixit96@gmail.com>
2018-02-05 11:59:15 +0530

    Grouping logic implemented

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,41 @@
+const https = require('https');
+
+
+function filterBasedOnAuthor(bookArray) {
+  const groupedDict = {};
+  bookArray.forEach((element) => {
+    if (groupedDict[element.Author] === undefined) {
+      groupedDict[element.Author] = [];
+      groupedDict[element.Author].push(element);
+    } else {
+      (groupedDict[element.Author]).push(element);
+    }
+  }, this);
+  console.log(groupedDict);
+  return groupedDict;
+}
+
+
+function jsonParser(data) {
+  const parsedJSON = JSON.parse(data);
+  const bookArray = parsedJSON.books;
+  return filterBasedOnAuthor(bookArray);
+}
+
+function getURLData(url) {
+  https.get(url, (response) => {
+    response.setEncoding('UTF8');
+    response.on('data', data => jsonParser(data));
+  });
+}
+
 module.exports =
   {
     method: 'GET',
     path: '/books',
     handler: (request, response) => {
-      response('Successful get');
+      const books = getURLData('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks');
+      response(books);
     },
   };
+
