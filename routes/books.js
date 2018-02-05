610c0aa4a507abb794ad9e91f9de3f233c90e017
krishna_dixit <rkdixit96@gmail.com>
2018-02-05 12:47:22 +0530

    API 1 logic implemented

--- routes/books.js
+++ routes/books.js
@@ -1,17 +1,27 @@
 const https = require('https');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
+function getBookRating(id) {
+  const xmlHttp = new XMLHttpRequest();
+  xmlHttp.open('GET', `https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${id}`, false); // false for synchronous request
+  xmlHttp.send(null);
+  const ratingObj = JSON.parse(xmlHttp.responseText);
+  return ratingObj.rating;
+}
 
 function filterBasedOnAuthor(bookArray) {
   const groupedDict = {};
-  bookArray.forEach((element) => {
-    if (groupedDict[element.Author] === undefined) {
-      groupedDict[element.Author] = [];
-      groupedDict[element.Author].push(element);
+  const bookData = bookArray.slice();
+  for (let book = 0; book < bookArray.length; book += 1) {
+    bookData[book].rating = getBookRating(bookData[book].id);
+    if (groupedDict[bookData[book].Author] === undefined) {
+      groupedDict[bookData[book].Author] = [];
+      groupedDict[bookData[book].Author].push(bookData[book]);
     } else {
-      (groupedDict[element.Author]).push(element);
+      (groupedDict[bookData[book].Author]).push(bookData[book]);
     }
-  }, this);
-  console.log(groupedDict);
+  }
+  // console.log(groupedDict);
   return groupedDict;
 }
 
@@ -19,14 +29,20 @@ function filterBasedOnAuthor(bookArray) {
 function jsonParser(data) {
   const parsedJSON = JSON.parse(data);
   const bookArray = parsedJSON.books;
+  // console.log(bookArray);
   return filterBasedOnAuthor(bookArray);
 }
 
+
 function getURLData(url) {
-  https.get(url, (response) => {
-    response.setEncoding('UTF8');
-    response.on('data', data => jsonParser(data));
-  });
+  const xmlHttp = new XMLHttpRequest();
+  xmlHttp.open('GET', url, false); // false for synchronous request
+  xmlHttp.send(null);
+  return jsonParser(xmlHttp.responseText);
+//   https.get(url, (response) => {
+//     response.setEncoding('UTF8');
+//     response.on('data', data => jsonParser(data));
+//   });
 }
 
 module.exports =
@@ -35,6 +51,7 @@ module.exports =
     path: '/books',
     handler: (request, response) => {
       const books = getURLData('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks');
+      console.log(books);
       response(books);
     },
   };
